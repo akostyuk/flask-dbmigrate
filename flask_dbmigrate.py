012cd847f401462e385861329033cb28cc75d587
Alexey Kostyuk <unitoff@gmail.com>
2013-01-12 00:41:45 +0400

    Added init and schemamigration methods. Updated tests

--- flask_dbmigrate.py
+++ flask_dbmigrate.py
@@ -1,6 +1,104 @@
+import os
+from shutil import rmtree
+
+from flask import current_app
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.script import Manager
+
+from sqlalchemy import schema
+
+from migrate.versioning import api, schemadiff
+
+
+class ImproperlyConfigured(Exception):
+    pass
 
 
 class DBMigrate(object):
 
     def __init__(self, app):
         self.app = app
+        self.sqlalchemy_database_uri = self._get_db_uri()
+        self.sqlalchemy_migration_path = self._get_migration_path()
+        self.db = self._get_db_engine()
+
+    def _get_db_uri(self):
+        if not 'SQLALCHEMY_DATABASE_URI' in self.app.config:
+            raise ImproperlyConfigured('Can not find '
+                'SQLALCHEMY_DATABASE_URI in application configuration')
+        else:
+            return self.app.config['SQLALCHEMY_DATABASE_URI']
+
+    def _get_migration_path(self):
+        if not 'SQLALCHEMY_MIGRATE_REPO' in self.app.config:
+            raise ImproperlyConfigured('Can not find '
+                'SQLALCHEMY_MIGRATE_REPO in application configuration')
+        else:
+            return self.app.config['SQLALCHEMY_MIGRATE_REPO']
+
+    def _get_db_engine(self):
+        try:
+            db = self.app.db
+            if not isinstance(db, SQLAlchemy):
+                raise ImproperlyConfigured('Can not find '
+                    'SQLAlchemy engine')
+            else:
+                return db
+        except AttributeError:
+            return SQLAlchemy(self.app)
+
+    def _is_changed(self, oldmodel, newmodel):
+        '''Check if the model has been changed'''
+
+        diff = schemadiff.SchemaDiff(oldmodel, newmodel)
+
+        if diff.tables_different:
+            return True
+        else:
+            return False
+
+    def _drop(self):
+        self.db.drop_all()
+        if os.path.exists(self.sqlalchemy_migration_path):
+            rmtree(self.sqlalchemy_migration_path)
+
+    def init(self):
+        self.db.create_all()
+        if not os.path.exists(self.sqlalchemy_migration_path):
+            api.create(self.sqlalchemy_migration_path, 'database repository')
+            api.version_control(self.sqlalchemy_database_uri,
+                self.sqlalchemy_migration_path)
+        else:
+            api.version_control(self.sqlalchemy_database_uri,
+                self.sqlalchemy_migration_path,
+                api.version(self.sqlalchemy_migration_path))
+
+    def schemamigrate(self):
+        old_model = schema.MetaData(bind=self.db.engine, reflect=True)
+        if 'migrate_version' in old_model.tables:
+            old_model.remove(old_model.tables['migrate_version'])
+        if not self._is_changed(old_model, self.db.metadata):
+            print('No Changes!')
+
+
+manager = Manager(usage='Perform database schema change management')
+
+
+@manager.command
+def test():
+    'Test command. Do nothing, just print "test ok"'
+    print('test ok')
+
+
+@manager.command
+def init():
+    'Initialize migration repository and create database'
+    dbmigrate = DBMigrate(current_app)
+    dbmigrate.init()
+
+
+@manager.command
+def schemamigration():
+    'Create migration'
+    dbmigrate = DBMigrate(current_app)
+    dbmigrate.schemamigrate()
--- tests.py
+++ tests.py
@@ -1,28 +1,36 @@
 import os
 import sys
 import unittest
+from shutil import rmtree
 
 from flask import Flask
 from flask.ext.script import Command, Manager
+from flask.ext.sqlalchemy import SQLAlchemy
 
-from .flask_dbmigrate import DBMigrate
+from flask_dbmigrate import DBMigrate
+from flask_dbmigrate import manager as dbmanager
+
+
+def rel(path):
+    return os.path.join(os.path.abspath(os.path.dirname(__file__)), path)
 
 
 def make_test_model(db):
     class Test(db.Model):
         __tablename__ = 'test'
         id = db.Column('test_id', db.Integer, primary_key=True)
-        value1 = db.Column(db.String(60))
+        column1 = db.Column(db.String(60))
 
-        def __init__(self, value1):
-            self.value1 = value1
+        def __init__(self, column1):
+            self.column1 = column1
     return Test
 
 
 class TestConfig(object):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite://'
-    SQLALCHEMY_MIGRATE_REPO = os.path.dirname(__file__)
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + rel('test.sqlite3')
+    # SQLALCHEMY_DATABASE_URI = 'sqlite://'
+    SQLALCHEMY_MIGRATE_REPO = rel('migrations')
 
 
 class TestCommand(Command):
@@ -34,10 +42,16 @@ class TestDBMigrateSubManager(unittest.TestCase):
     def setUp(self):
         self.app = Flask(__name__)
         self.app.config.from_object(TestConfig)
+        self.app.db = SQLAlchemy(self.app)
+        self.Test = make_test_model(self.app.db)
         self.dbmigrate = DBMigrate(self.app)
 
     def tearDown(self):
-        self.db.drop_all()
+        self.dbmigrate.db.drop_all()
+        if os.path.exists(self.app.config['SQLALCHEMY_MIGRATE_REPO']):
+            rmtree(self.app.config['SQLALCHEMY_MIGRATE_REPO'])
+        if os.path.exists(rel('test.sqlite3')):
+            os.remove(rel('test.sqlite3'))
 
     def test_add_dbmigrate_submanager(self):
         # check settings first
@@ -54,26 +68,64 @@ class TestDBMigrateSubManager(unittest.TestCase):
             manager.get_options())
 
     def test_run_dbmigrate_submanager_test_command(self):
-        dbmigrate_manager = Manager()
-        dbmigrate_manager.add_command('test', TestCommand())
-
         manager = Manager(self.app)
-        manager.add_command('dbmigrate', dbmigrate_manager)
+        manager.add_command('dbmigrate', dbmanager)
 
-        sys.argv = ["manage.py", "dbmigrate", "test"]
+        sys.argv = ['manage.py', 'dbmigrate', 'test']
 
         try:
             manager.run()
         except SystemExit, e:
             self.assertEquals(e.code, 0)
 
-        if not hasattr(sys.stdout, "getvalue"):
-            self.fail("need to run in buffered mode")
+        if not hasattr(sys.stdout, 'getvalue'):
+            self.fail('need to run in buffered mode')
         output = sys.stdout.getvalue().strip()
         self.assertEquals(output, 'test ok')
 
-    def test_run_dbmigrate_submanager_dbcreate_command(self):
-        pass
+    def test_run_dbmigrate_init(self):
+        self.dbmigrate._drop()
+        manager = Manager(self.app)
+        manager.add_command('dbmigrate', dbmanager)
+
+        sys.argv = ['manage.py', 'dbmigrate', 'init']
+
+        try:
+            manager.run()
+        except SystemExit, e:
+            self.assertEquals(e.code, 0)
+
+        self.assertTrue(os.path.exists(
+            self.app.config['SQLALCHEMY_MIGRATE_REPO']))
+
+        # test if table test exist
+        self.assertEqual(self.app.db.metadata.tables['test'].name,
+            'test')
+
+        # test insert
+        test = self.Test('Test')
+        self.app.db.session.add(test)
+        self.app.db.session.commit()
+        self.assertEqual(len(self.Test.query.all()), 1)
+
+        # drop
+        self.dbmigrate._drop()
+
+    def test_run_dbmigrate_schemamigrate_no_changes(self):
+        self.dbmigrate.init()
+
+        manager = Manager(self.app)
+        manager.add_command('dbmigrate', dbmanager)
+
+        sys.argv = ['manage.py', 'dbmigrate', 'schemamigration']
+
+        try:
+            manager.run()
+        except SystemExit, e:
+            self.assertEquals(e.code, 0)
+
+        output = sys.stdout.getvalue().strip()
+        self.assertEquals(output, 'No Changes!')
 
 if __name__ == '__main__':
     assert not hasattr(sys.stdout, 'getvalue')
